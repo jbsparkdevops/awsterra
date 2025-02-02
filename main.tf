b99b832344e080a43009469fd5091a05ee5aa74f
jbsparkdevops <bharghav588@gmail.com>
2025-02-01 20:40:36 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+
+# Variables
+
+variable "server_name" {
+        default = "test-server1"
+}
+
+
+resource "aws_instance" "myserver" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t2.micro"
+  key_name = "Controller_key"
+  user_data = <<EOF
+#!/bin/bash
+sudo yum install httpd -y
+sudo systemctl enable httpd
+echo "Hello, Welcome to Terraform" > /var/www/html/index.html
+sudo systemctl restart httpd
+EOF
+  tags = {
+    Name = "${var.server_name}"
+  }
+}
+
+
+output "mypublicip" {
+        value = "${aws_instance.myserver.public_ip}"
+
+}
